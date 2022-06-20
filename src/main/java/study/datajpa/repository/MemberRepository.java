eb27d21be5963b6693c8185873ec565bb621187f
eomjunseong <hi_man159@naver.com>
2022-06-20 14:18:05 +0900

    repo에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -14,7 +14,11 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 //    @Query(name = "Member.findByUsername") //when Param?? --> :username //jpql 에 명확히 있을때
 //    위에꺼 생략해도 작동하는데 .... -> JpaRepository<Membe,> 보고, Member.findByUsername 로 Named쿼리 먼저 찾고, 없으면 메소드 이름으로 쿼리~~~ 규칙 찾기 때문임
 //    이기능 잘 안함 : Repository 에 바로 쿼리 작성하는 기능이 있어서 ,
-//    그냥 이게 있구나~ 정도만 알고 넘어가기 
+//    그냥 이게 있구나~ 정도만 알고 넘어가기
     List<Member> findByUsername(@Param("username") String username);
+
+//    Reposoitory에 바로 쿼리 쓰는법
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username")String username, @Param("age")int age);
     
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -14,17 +14,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Rollback(false)
+//@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired
-    MemberRepository MemberRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void testMember(){
         Member member = new Member("eom1");
-        Member save = MemberRepository.save(member);
-        Member findMember = MemberRepository.findById(member.getId()).get();
+        Member save = memberRepository.save(member);
+        Member findMember = memberRepository.findById(member.getId()).get();
 
         assertThat(findMember.getId()).isEqualTo(save.getId());
         assertThat(findMember.getId()).isEqualTo(save.getId());
@@ -34,31 +34,31 @@ class MemberRepositoryTest {
     public void basicCRUD() {
         Member member1 = new Member("member1");
         Member member2 = new Member("member2");
-        MemberRepository.save(member1);
-        MemberRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
 
 
         //단건 조회 검증
-        Member findMember1 = MemberRepository.findById(member1.getId()).get();
-        Member findMember2 = MemberRepository.findById(member2.getId()).get();
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
 
         assertThat(findMember1).isEqualTo(member1);
         assertThat(findMember2).isEqualTo(member2);
 
         //리스트 조회 검증
-        List<Member> all = MemberRepository.findAll();
+        List<Member> all = memberRepository.findAll();
         assertThat(all.size()).isEqualTo(2);
 
         //카운트 검증
-        long count = MemberRepository.count();
+        long count = memberRepository.count();
         assertThat(count).isEqualTo(2);
 
         //삭제 검증
-        MemberRepository.delete(member1);
-        MemberRepository.delete(member2);
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
 
-        long deletedCount = MemberRepository.count();
+        long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
     @Test
@@ -66,10 +66,10 @@ class MemberRepositoryTest {
         Member member1 = new Member("aaa", 10);
         Member member2 = new Member("aaa", 20);
 
-        MemberRepository.save(member1);
-        MemberRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
-        List<Member> findMembers = MemberRepository.findByUsernameAndAgeGreaterThan("aaa", 19);
+        List<Member> findMembers = memberRepository.findByUsernameAndAgeGreaterThan("aaa", 19);
         Member findMember = findMembers.get(0);
         assertThat(findMember.getAge()).isEqualTo(20);
 
@@ -79,10 +79,22 @@ class MemberRepositoryTest {
         Member member1 = new Member("AAA", 10);
         Member member2 = new Member("BBB", 20);
 
-        MemberRepository.save(member1);
-        MemberRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
-        List<Member> aaa = MemberRepository.findByUsername(member1.getUsername());
+        List<Member> aaa = memberRepository.findByUsername(member1.getUsername());
         assertThat(aaa.get(0).getUsername()).isEqualTo(member1.getUsername());
+    }
+
+    @Test
+    public void queryTest(){
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findUser("AAA",10);
+        assertThat(member1).isEqualTo(result.get(0));
     }
 }
